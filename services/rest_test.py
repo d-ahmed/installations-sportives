c92954e6eb8f45c5ca0df196dc889e93452bfaec
Daniel <el@la.fr>
2015-03-06 19:22:42 +0100

    quelques modifications sur la classe et service rest

--- services/rest_test.py
+++ services/rest_test.py
@@ -5,7 +5,7 @@ import mysql.connector as mysql
 from mysql.connector import Error
 import sys
 sys.path.append("/home/daniel/Documents/Creation_D_application/with_mysql/model")
-bottle.TEMPLATE_PATH.insert(0, "/home/daniel/Documents/Creation_D_application/with_mysql/services/vue/html/")
+bottle.TEMPLATE_PATH.insert(0, "/home/daniel/Documents/Creation_D_application/with_mysql/vue/html/")
 
 import Dao
 
--- vue/html/debut.tpl
+++ vue/html/debut.tpl
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Installation Sportive</title>
 	<link rel="stylesheet" type="text/css" href="../css/layout.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<meta charset="utf-8">
+	<title>Installation Sportive</title>
 </head>
 <body>
 	<div id="contenue">
