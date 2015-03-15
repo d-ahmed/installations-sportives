da1b025a5a67e033d0409db16e22c92376fe6e04
Daniel <el@la.fr>
2015-03-15 17:21:31 +0100

    update service and vue

--- vue/js/installations.js
+++ vue/js/installations.js
@@ -15,8 +15,9 @@ $(document).ready(function(){
 		$("<input/>",{type:'text', id:'activite'}).appendTo($('#p2'));
 		$("<input/>",{type:'submit', value:'recherche'}).appendTo($('#col3'));
 		*/
-
+		
 		$('#recherche').submit(function(e){
+			e.preventDefault();
 			if($('#error')){
 				$('#error').remove();
 			}	
@@ -28,17 +29,31 @@ $(document).ready(function(){
 			$('#ville').val("");
 			$('#activite').val("");
 			//$('#recherche').css({display:'none'});
+			if($(".reponce")){
+				$(".reponce").remove();
+			}
 			$.ajax({
-				type: 'GET',
-				url:'http://localhost:8080/installation'+requette,
-				dataType: 'json',
-				success: function (data) {
+				url:"http://localhost:8001/installation"+requette,
+				dataType: 'JSON',
+    			//jsonpCallback: 'callback',
+    			//type: 'GET',
+				success: function (responce) {
 					$('#error').hide();
-					$('<p/>',{text:data.numero,class:'reponce'}).appendTo($("#container"));
-					$('<p/>',{text:data.nom,class:'reponce'}).appendTo($("#container"));
+					$.each(responce, function(index,responce) {
+						$.each(responce,function(i){
+						//console.log(responce[i].installation);
+						$('<div/>',{id:""+(i),class:"reponce"}).appendTo($('#container'));
+						$('<div/>',{class:"panel panel-primary"}).appendTo($("#"+(i)));
+						$('<div/>',{class:"panel-heading"}).text(responce[i].installation+" "+responce[i].code_postal+" "+responce[i].ville).appendTo($("#"+(i)).find('.panel-primary'));
+						$('<div/>',{class:"panel-body"}).appendTo($("#"+(i)).find('.panel-primary'));
+						$('<p/>').text("Activite : "+responce[i].activite).appendTo($("#"+(i)).find('.panel-body'));
+						$('<p/>').text("Adresse : "+responce[i].adresse+" "+responce[i].code_postal+" "+responce[i].ville).appendTo($("#"+(i)).find('.panel-body'));
+						})
+					})
 				},
 
 				error: function (data, xhr, status, err) {
+					 console.log(data);
 					$('#recherche').find('div').each(function(i){
 						if(i<2){
 							$(this).addClass('has-error');
@@ -46,9 +61,13 @@ $(document).ready(function(){
 					});
 					$('<div/>',{id:'error', class:'col-md-12'}).appendTo('#container');
 					$('<p/>').text("Erreur lors du chargement de l'installation ...").appendTo($('#error'));
-				}
+				}, 
 			});
-			e.preventDefault();
+			
 		});
 	//});
 });
+
+function base(arg){
+	console.log(arg);
+}
